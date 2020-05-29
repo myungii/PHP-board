67f400ce6953a331d0284783bbf71f34d1217bf1
myungii <myung_gii@naver.com>
2020-05-29 16:28:10 +0900

    200529 git test

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title></title>
+	<script type="text/javascript">
+		setTimeout( "location.href='#' ", 3000) ; 
+	</script>
+</head>
+<body>
+
+	<div align="center">
+		<font size=6 color=blue><b> Waiting...Loading...</b></font> <p>
+	   <img src="resources/images/tulips.png" width="500" height="450">
+	</div>
+</body>
+</html>
